033e51651acd71413580a93ce60cfa565c187dac
Wolfgang Schultz <woschultz@about.me>
2017-05-31 19:28:56 +0200

    db => dB

--- updatePanelTitle.m
+++ updatePanelTitle.m
@@ -5,4 +5,4 @@ Frequenz = handles.uibuttongroup2.SelectedObject.String;
 
 HearLevel = handles.slider1.UserData;
 
-set (handles.uipanel1,'Title', horzcat('Audiogramm     ',' ', num2str(HearLevel),' ','db    ',Frequenz));
+set (handles.uipanel1,'Title', horzcat('Audiogramm     ',' ', num2str(HearLevel),' ','dB    ',Frequenz));
